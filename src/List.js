c42084413e6d128e1e05b61f01162806a399727a
gianlucanied <gianluca.nieddu96@gmail.com>
2024-04-16 17:17:30 +0200

    Create: app

---
+++ src/List.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+//React-Icons
+//Spiegare parentesi prima dopo arrow che significa che si sta returnandp
+//Passare funzioni ai componenti
+
+const List = (props) => {
+  return (
+    <>
+      <ul className="user-list">
+        {props.data.map((person) => (
+          <li key={person.id}>
+            {" "}
+            <Person {...person} removeItem={props.removeItem} />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+const Person = ({ id, nome, stato, img, removeItem }) => {
+  return (
+    <article>
+      <img src={img} alt="prs" className="person-img" />
+      <div className="person-info">
+        <div className="person-action">
+          <h4>{nome}</h4>
+          <button className="btn" onClick={() => removeItem(id)}>
+            X
+          </button>
+        </div>
+        <p>{stato}</p>
+      </div>
+    </article>
+  );
+};
+
+export default List;
